e1d0fe72b92bc6d6ec432b720f28f3c2b3ab4d28
Alyssa <alyssalareyes@gmail.com>
2025-08-08 17:53:42 -0700

    finished main layout for landing page

--- vnza-station/src/App.jsx
+++ vnza-station/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import LandingPage from './components/LandingPage/LandingPage'
 
@@ -9,11 +8,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-        <LandingPage/>
-     
-      
+    <>     
+      <LandingPage/>  
     </>
   )
 }
--- vnza-station/src/components/LandingPage/LandingPage.jsx
+++ vnza-station/src/components/LandingPage/LandingPage.jsx
@@ -6,12 +6,17 @@ import CenteredPage from '../CenteredPage/CenteredPage'
 function LandingPage(){
     return(
         <CenteredPage>
-            <p className="text-white">
+            <p className="text-white text-sm m-0 p-0">
                 welcome to
             </p>
-            <p className="text-white">
+            <p className="text-white text-[50px] m-0 p-0">
                 VNZA 102.5
             </p>
+            <div className="absolute bottom-0 w-full h-1/2 overflow-hidden flex justify-center items-start pt-10">
+               <img src="/cartoon-me.svg" alt="cartoon drawing of me"/>
+               
+            </div>
+            
         </CenteredPage>
     );
 }
